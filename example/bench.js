78f50196ba430c376da3e0785e545511f2f9892b
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-04-12 18:46:50 +0200

    benchmark script

---
+++ example/bench.js
@@ -0,0 +1,36 @@
+var umkv = require('../')
+var db = require('level')('/tmp/kv.db')
+var kv = umkv(db)
+var randomBytes = require('crypto').randomBytes
+
+var batchSize = Number(process.argv[2])
+var times = Number(process.argv[3])
+
+var keys = []
+for (var i = 0; i < 5000; i++) {
+  keys.push(randomBytes(4).toString('hex'))
+}
+
+var start = Date.now()
+var uid = 0
+
+;(function next (n) {
+  if (n === times) return finish()
+  var docs = []
+  for (var i = 0; i < batchSize; i++) {
+    docs.push({
+      id: uid,
+      key: keys[Math.floor(Math.random()*keys.length)],
+      links: uid > 0 ? [uid-1] : []
+    })
+  }
+  kv.batch(docs, function (err) {
+    if (err) return console.error(err)
+    else next(n+1)
+  })
+})(0)
+
+function finish () {
+  var elapsed = Date.now() - start
+  console.log(elapsed)
+}
